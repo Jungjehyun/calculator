43aabb151bb7ba5cb682aed0217f73550f13044f
Jungjehyun <toojey7@gmail.com>
2018-11-28 12:07:31 +0900

    Add function included

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello Calculator\n");
+	printf("두 수를 입력하세요.\n");
+	printf("Hello Calculator\n");
 
+	scanf("%d %d",&n1,&n2);
+	printf("%d + %d = %d\n",n1+n2);
     return 0;
 }
